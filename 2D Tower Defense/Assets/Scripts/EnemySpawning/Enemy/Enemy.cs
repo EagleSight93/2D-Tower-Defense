469aa9ce2fa2002a7b7fb823c5114091356713b4
NuiN99 <endyverse@gmail.com>
2023-10-29 20:35:31 -0700

    Added NavMesh2D to enemies

--- 2D Tower Defense/Assets/Scripts/EnemySpawning/Enemy/Enemy.cs
+++ 2D Tower Defense/Assets/Scripts/EnemySpawning/Enemy/Enemy.cs
@@ -1,9 +1,26 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Profiling.Memory.Experimental;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour, IDestructable, ITargetable
 {
+    NavMeshAgent _agent;
+
+    void Awake()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+        _agent.updateRotation = false;
+        _agent.updateUpAxis = false;
+    }
+
+    void Start()
+    {
+        _agent.SetDestination(Vector3.zero);
+    }
+
     void IDestructable.Damaged(float damageTaken) { }
     void IDestructable.Destroyed() => Destroy(gameObject);
 
